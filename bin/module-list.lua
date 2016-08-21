d361da1fb6092b6f39183db49352aa5757929a1f
Aaron Shaw <robothacker@gmail.com>
2016-08-20 23:02:31 -0700

    Update module-list.lua

--- bin/module-list.lua
+++ bin/module-list.lua
@@ -1,7 +1,7 @@
 return function (connection, req, args)
   dofile('httpserver-header.lc')(connection, 200, 'html')
 
-  connection:send('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>WiFi info</title></head><body>')
+  connection:send('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>WiFi info</title></head><body><ul>')
 
   local modulelist = {};
   modulelist[ "core"      ] = { "test" : true             , "docurl" : "http://www.lua.org/manual/5.1/manual.html" };
@@ -9,61 +9,57 @@ return function (connection, req, args)
   modulelist[ "string"    ] = { "test" : string    ~= nil , "docurl" : "http://www.lua.org/manual/5.1/manual.html#5.4" };
   modulelist[ "table"     ] = { "test" : table     ~= nil , "docurl" : "http://www.lua.org/manual/5.1/manual.html#5.5" };
   
-  modulelist[ "adc"       ] = { "test" : adc       ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/master/en/modules/adc/" };
-  modulelist[ "adxl345"   ] = { "test" : adxl345   ~= nil , "docurl" : "" };
+  modulelist[ "adc" ] = { "test" : adc ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/adc/" };
+  modulelist[ "adxl345" ] = { "test" : adxl345 ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/adxl345/" };
+  modulelist[ "am2320" ] = { "test" : am2320 ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/am2320/" };
+  modulelist[ "apa102" ] = { "test" : apa102 ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/apa102/" };
+  modulelist[ "bit" ] = { "test" : bit ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/bit/" };
+  modulelist[ "bme280" ] = { "test" : bme280 ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/bme280/" };
+  modulelist[ "bmp085" ] = { "test" : bmp085 ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/bmp085/" };
+  modulelist[ "cjson" ] = { "test" : cjson ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/cjson/" };
+  modulelist[ "coap" ] = { "test" : coap ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/coap/" };
+  modulelist[ "crypto" ] = { "test" : crypto ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/crypto/" };
+  modulelist[ "dht" ] = { "test" : dht ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/dht/" };
+  modulelist[ "encoder" ] = { "test" : encoder ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/encoder/" };
+  modulelist[ "enduser_setup" ] = { "test" : enduser_setup ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/enduser_setup/" };
+  modulelist[ "file" ] = { "test" : file ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/file/" };
+  modulelist[ "gpio" ] = { "test" : gpio ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/gpio/" };
+  modulelist[ "hmc5883l" ] = { "test" : hmc5883l ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/hmc5883l/" };
+  modulelist[ "http" ] = { "test" : http ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/http/" };
+  modulelist[ "hx711" ] = { "test" : hx711 ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/hx711/" };
+  modulelist[ "i2c" ] = { "test" : i2c ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/i2c/" };
+  modulelist[ "l3g4200d" ] = { "test" : l3g4200d ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/l3g4200d/" };
+  modulelist[ "mdns" ] = { "test" : mdns ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/mdns/" };
+  modulelist[ "mqtt" ] = { "test" : mqtt ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/mqtt/" };
+  modulelist[ "net" ] = { "test" : net ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/net/" };
+  modulelist[ "node" ] = { "test" : node ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/node/" };
+  modulelist[ "ow" ] = { "test" : ow ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/ow/" };
+  modulelist[ "pcm" ] = { "test" : pcm ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/pcm/" };
+  modulelist[ "perf" ] = { "test" : perf ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/perf/" };
+  modulelist[ "pwm" ] = { "test" : pwm ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/pwm/" };
+  modulelist[ "rc" ] = { "test" : rc ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/rc/" };
+  modulelist[ "rotary" ] = { "test" : rotary ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/rotary/" };
+  modulelist[ "rtcfifo" ] = { "test" : rtcfifo ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/rtcfifo/" };
+  modulelist[ "rtcmem" ] = { "test" : rtcmem ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/rtcmem/" };
+  modulelist[ "rtctime" ] = { "test" : rtctime ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/rtctime/" };
+  modulelist[ "sigma_delta" ] = { "test" : sigma_delta ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/sigma_delta/" };
+  modulelist[ "sntp" ] = { "test" : sntp ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/sntp/" };
+  modulelist[ "spi" ] = { "test" : spi ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/spi/" };
+  modulelist[ "struct" ] = { "test" : struct ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/struct/" };
+  modulelist[ "switec" ] = { "test" : switec ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/switec/" };
+  modulelist[ "tm1829" ] = { "test" : tm1829 ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/tm1829/" };
+  modulelist[ "tmr" ] = { "test" : tmr ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/tmr/" };
+  modulelist[ "tsl2561" ] = { "test" : tsl2561 ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/tsl2561/" };
+  modulelist[ "u8g" ] = { "test" : u8g ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/u8g/" };
+  modulelist[ "uart" ] = { "test" : uart ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/uart/" };
+  modulelist[ "ucg" ] = { "test" : ucg ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/ucg/" };
+  modulelist[ "wifi" ] = { "test" : wifi ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/wifi/" };
+  modulelist[ "ws2801" ] = { "test" : ws2801 ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/ws2801/" };
+  modulelist[ "ws2812" ] = { "test" : ws2812 ~= nil , "docurl" : "http://nodemcu.readthedocs.io/en/dev/en/modules/ws2812/" };
 
-  connection:send('<p><b>core:</b> ') if core ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>coroutine:</b> ') if coroutine ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>string:</b> ') if string ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>table:</b> ') if table ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
+  for k,v in pairs(modulelist) do
+    connection:send('<li><b><a href="'..modulelist[ k ][ "docurl" ]..'">'..k..'</a></b> ') if modulelist[ k ][ "test" ] then connection:send('yes') else connection:send('no') end connection:send('</li>')
+  end
 
-  connection:send('<p><b>adc:</b> ') if adc ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>adxl345:</b> ') if adxl345 ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>am2320:</b> ') if am2320 ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>apa102:</b> ') if apa102 ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>bit:</b> ') if bit ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>bme280:</b> ') if bme280 ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>bmp085:</b> ') if bmp085 ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>cjson:</b> ') if cjson ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>coap:</b> ') if coap ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>crypto:</b> ') if crypto ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>dht:</b> ') if dht ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>encoder:</b> ') if encoder ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>enduser_setup:</b> ') if enduser_setup ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>file:</b> ') if file ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>gpio:</b> ') if gpio ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>hmc5883l:</b> ') if hmc5883l ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>http:</b> ') if http ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>hx711:</b> ') if hx711 ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>i2c:</b> ') if i2c ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>l3g4200d:</b> ') if l3g4200d ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>mdns:</b> ') if mdns ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>mqtt:</b> ') if mqtt ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>net:</b> ') if net ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>node:</b> ') if node ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>ow:</b> ') if ow ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>pcm:</b> ') if pcm ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>perf:</b> ') if perf ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>pwm:</b> ') if pwm ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>rc:</b> ') if rc ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>rotary:</b> ') if rotary ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>rtcfifo:</b> ') if rtcfifo ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>rtcmem:</b> ') if rtcmem ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>rtctime:</b> ') if rtctime ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>sigma_delta:</b> ') if sigma_delta ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>sntp:</b> ') if sntp ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>spi:</b> ') if spi ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>struct:</b> ') if struct ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>switec:</b> ') if switec ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>tm1829:</b> ') if tm1829 ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>tmr:</b> ') if tmr ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>tsl2561:</b> ') if tsl2561 ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>u8g:</b> ') if u8g ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>uart:</b> ') if uart ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>ucg:</b> ') if ucg ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>wifi:</b> ') if wifi ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>ws2801:</b> ') if ws2801 ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-  connection:send('<p><b>ws2812:</b> ') if ws2812 ~= nil then connection:send('yes') else connection:send('no') end connection:send('</p>')
-
-  connection:send('</body></html>')
+  connection:send('</ul></body></html>')
 end
